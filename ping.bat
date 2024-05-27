06eb4577e243c15a39a4c71b798469815d87fef9
Danko13-ui <68393763+Danko13-ui@users.noreply.github.com>
2024-05-27 22:56:59 +0700

    Create ping.bat

---
+++ ping.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Pinging RDP, So RDP Cant Die...
+:spamming
+ping 127.0.0.1 > null
+cls
+echo PINGED !!
+sleep 50
+              break
+          
+          echo "PINGED !!"
+          break
+          sleep 30
+          goto spamming
+          break
+          sleep 30
+          timeout 360
